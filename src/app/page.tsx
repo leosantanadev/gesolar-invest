86f9a24a6181a8ee0f47b9fa2b055c84aafa22f8
leosantanadev <ls07062005@gmail.com>
2024-10-10 10:41:25 -0300

    cases section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <AboutUsSection />
       <StatsSection />
       <HowWeWorkSection />
-      {/* <CasesSection /> */}
+      <CasesSection />
       <PrincingSection />
       {/* <FAQSection /> */}
 
